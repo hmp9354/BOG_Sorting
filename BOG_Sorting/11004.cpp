9e1d9736deb9c75772b873096eba79997ef697cc
nirip <phm0259@gmail.com>
2022-03-15 15:09:04 +0900

    22/03/15

--- BOG_Sorting/11004.cpp
+++ BOG_Sorting/11004.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
+#include <algorithm>
+using namespace std;
 
 int main() {
-	long long N;
-	long long K;
-	scanf_s("%lld %lld", &N, &K);
+	int N;
+	int K;
+	scanf_s("%d %d", &N, &K);
 
-	for (long long i = 0; i < N; i++) {
-
+	int* list = new int[N];
+	for (int i = 0; i < N; i++) {
+		scanf_s("%d", &list[i]);
 	}
+
+	sort(list, list + N);
+
+	printf("%d", list[K - 1]);
 }
